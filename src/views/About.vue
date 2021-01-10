2053c8ddcba194f5ddc7bcb3595f12940daddf09
VivianaGuerraCustodio <vivianaguerracustodio@gmail.com>
2021-01-10 17:33:45 -0500

    🚀

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">Platzi Exchange v1.0.0</h1>
+    <p>
+      Proyecto para obtener las cotizaciones de las cryptomonedas más
+      importantes a traves de una API REST de coincap
+      <br />
+      Este proyecto es utilizado en el curso básico de Vue js de Platzi
+    </p>
+    <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline"
+      >Volver a la página de inicio</router-link
+    >
+  </div>
+</template>
